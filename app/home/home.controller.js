5d8793237a7f02cc390e455e24396f062db81d1f
Swati Kansal <Swati.Kansal@sabre.com>
2020-11-30 18:56:40 +0530

    Initial commit

---
+++ app/home/home.controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+  'use strict';
+
+  angular
+      .module('app')
+      .controller('HomeController', HomeController);
+
+  HomeController.$inject = ['$rootScope'];
+  function HomeController($rootScope) {
+    var vm = this;
+
+    vm.user = null;
+       initController();
+
+    function initController() {
+      loadCurrentUser();
+    }
+
+    function loadCurrentUser() {
+            var user = {};
+             user.username = 'admin';
+             user.firstName = 'Swati';
+             user.lastName = 'Kansal';
+            vm.user = user;
+    }
+
+  }
+
+})();
